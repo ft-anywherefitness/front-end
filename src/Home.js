5bd831d7d8f016d0e9aab3bd5dbec7e8eb090caa
TrevourGuild <trevorr.guildd@gmail.com>
2021-07-27 13:02:31 -0700

    adjusted home and added schema

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,16 @@ export default function Home(){
         <div className = 'home-page'>
             <header className = 'home-header'>
                 <h1>Anywhere Fitness</h1>
+
+                <Link to= '/'>
+                <button>Home</button>
+                </Link>
+
                 <Link to= '/Login'>
                 <button>Login</button>
                 </Link>
+
+                
             </header>
         </div>
     )
