b33ab3b170ce0ee686d48325ff0a4c20d37015c3
escttsalo <escttsalo@gmail.com>
2021-07-29 14:59:55 -0600

    home tweaks

--- src/App.js
+++ src/App.js
@@ -4,9 +4,37 @@ import Home from './components/Home';
 import Client from './components/Client';
 import Register from './components/Register';
 import Instructor from './components/Instructor';
-import {Route, Switch} from 'react-router-dom'
-
+import {Route, Link, Switch} from 'react-router-dom'
+import styled from 'styled-components'
 import { useState } from 'react'
+
+
+const StyledHome = styled.nav`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  img{
+      margin-top: 2%;
+      width: 70%;
+  }
+  button{
+      margin: 1.2%;
+      padding: 0.5%;
+      background-color: white;
+      justify-content:center;
+      border-radius:5px;
+      background-color:#ccccff;
+      width:15%
+  }
+  button:hover{
+      background-color: grey;
+      color: white;
+  }
+  p{
+      margin: 2%;
+  }
+`
+
 
 function App() {
 
@@ -14,6 +42,34 @@ function App() {
 
   return (
     <div className = 'header'>
+      <StyledHome>
+        <div className = 'home-page'>
+            <header className = 'home-header'>
+                <h1>Anywhere Fitness</h1>
+
+                <Link to= '/'>
+                <button>Home</button>
+                </Link>
+
+                <Link to= '/Login'>
+                <button>Login</button>
+                </Link>
+
+                <Link to = '/Register'>
+                <button>Register</button>
+                </Link>
+
+                <Link to= '/Client'>
+                <button>Client</button>
+                </Link>
+
+                <Link to = '/Instructor'>
+                <button>Instructor</button> 
+                </Link>
+            </header>
+        </div>
+    </StyledHome>
+
     <Switch>
 
       <Route exact path = '/'>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,68 +1,55 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
+
 const StyledHome = styled.div`
-display: flex;
-flex-direction: column;
-flex-wrap: wrap;
-img{
-    margin-top: 2%;
-    width: 70%;
-}
-button{
-    margin: 1.2%;
-    padding: 0.5%;
-    background-color: white;
-    justify-content:center;
-    border-radius:5px;
-    background-color:#ccccff;
-    width:15%
-}
-button:hover{
-    background-color: grey;
-    color: white;
-}
-p{
-    margin: 2%;
-}
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    img{
+        /* margin-top: 2%; */
+        margin: 6% auto 0;
+        width: 70%;
+    }
+    p{
+        margin: 2%;
+    }
 `
+// const StyledHome = styled.div`
+// display: flex;
+// flex-direction: column;
+// flex-wrap: wrap;
+// img{
+//     margin-top: 2%;
+//     width: 70%;
+// }
+// button{
+//     margin: 1.2%;
+//     padding: 0.5%;
+//     background-color: white;
+//     justify-content:center;
+//     border-radius:5px;
+//     background-color:#ccccff;
+//     width:15%
+// }
+// button:hover{
+//     background-color: grey;
+//     color: white;
+// }
+// p{
+//     margin: 2%;
+// }
+// `
 
 
 export default function Home(){
     
 
     return (
-    <StyledHome>
-        <div className = 'home-page'>
-            <header className = 'home-header'>
-                <h1>Anywhere Fitness</h1>
-
-                <Link to= '/'>
-                <button>Home</button>
-                </Link>
-
-                <Link to= '/Login'>
-                <button>Login</button>
-                </Link>
-
-                <Link to = '/Register'>
-                <button>Register</button>
-                </Link>
-
-                <Link to= '/Client'>
-                <button>Client</button>
-                </Link>
-
-                <Link to = '/Instructor'>
-                <button>Instructor</button> 
-                </Link>
-            </header>
-            <img src = 'https://images.unsplash.com/photo-1558611848-73f7eb4001a1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGZpdG5lc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'></img>
+        <StyledHome>
+            <img src = 'https://images.unsplash.com/photo-1558611848-73f7eb4001a1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGZpdG5lc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60' alt='weights'></img>
 
             <p>These days, fitness classes can be held anywhere- a park, an unfinished basement or a garage- not just at a traditional gym. Certified fitness instructors need an easy way to take the awkwardness out of attendance taking and client payment processing.</p>
-
-        </div>
         </StyledHome>
     )
 }
